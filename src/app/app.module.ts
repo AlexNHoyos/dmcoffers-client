417dd4b63ac38e745edd810fd9535f54e1e53c9c
Ramiro Ceballos <rceballos.work@gmail.com>
2024-08-03 15:42:26 -0300

    comienzo de componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserRegisterComponent } from './user-register/user-register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    UserRegisterComponent,
   ],
   imports: [
     BrowserModule,
