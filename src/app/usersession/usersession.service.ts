caa02973ca7d090060db8efd1fe1d648cf942882
Ramiro Ceballos <rceballos.work@gmail.com>
2024-09-02 23:14:55 -0300

    update user service

--- src/app/usersession/usersession.service.ts
+++ src/app/usersession/usersession.service.ts
@@ -1,10 +1,39 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../shared/models/user.js';
+import { catchError, Observable, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
+export class UserService {
+  constructor(private http: HttpClient) { }
 
-export class UserSessionService {
+  getUser(id: number): Observable<User> {
+    return this.http
+      .get<User>(environment.urlApi + 'users/' + id)
+      .pipe(catchError(this.handleError));
+  }
 
-  constructor() { }
+  updateUser(id: number, userRequest: User): Observable<any> {
+    return this.http
+      .put<User>(environment.urlApi + 'users/' + id, userRequest)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error('Se ha producido un error ', error.error);
+    } else {
+      console.error(
+        'Backend retorno el codigo de estado ',
+        error.status,
+        error.error
+      );
+    }
+    return throwError(
+      () => new Error('Algo fallo. Por favor intente nuevamente.')
+    );
+  }
 }
