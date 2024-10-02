80ae497ff3cdeaede3a3a4670ae7c8953b3a311e
Mauro Marcosano <mauromarcosano@gmail.com>
2024-10-02 17:14:44 -0300

    Agregar bundle analizer

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,18 @@
+const path = require("path");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
 console.log("Webpack custom config is loaded!");
+
 module.exports = {
+  plugins: [
+    new BundleAnalyzerPlugin({
+      generateStatsFile: true,
+      reportFilename: "bundle-report.html",
+    }),
+  ],
   output: {
+    path: path.resolve(__dirname, "dist/dmcoffers-client"),
     filename: "[name].[contenthash].js",
   },
 };
