6fc3c87822d46db5743e09cd26a843095b9992aa
Mauro Marcosano <mauromarcosano@gmail.com>
2024-10-03 01:18:10 -0300

    Agregar speed measure plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,12 @@ const path = require("path");
 const BundleAnalyzerPlugin =
   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+const smp = new SpeedMeasurePlugin();
+
 console.log("Webpack custom config is loaded!");
 
-module.exports = {
+module.exports = smp.wrap({
   plugins: [
     new BundleAnalyzerPlugin({
       generateStatsFile: true,
@@ -15,4 +18,4 @@ module.exports = {
     path: path.resolve(__dirname, "dist/dmcoffers-client"),
     filename: "[name].[contenthash].js",
   },
-};
+});
