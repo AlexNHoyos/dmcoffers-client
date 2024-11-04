4f56ac3ed278cfc3df37780bbe4c4caa32005580
Mauro Marcosano <mauromarcosano@gmail.com>
2024-11-04 10:53:54 -0300

    Arreglado manejo de sesion cuando se expira el token

--- src/app/services/auth/jwt-interceptor.service.ts
+++ src/app/services/auth/jwt-interceptor.service.ts
@@ -1,18 +1,20 @@
 import {
+  HttpErrorResponse,
   HttpEvent,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http/index.js';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 import { LoginService } from './login.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class JwtInterceptorService implements HttpInterceptor {
-  constructor(private loginService: LoginService) {}
+  constructor(private loginService: LoginService, private router: Router) {}
 
   intercept(
     req: HttpRequest<any>,
@@ -20,7 +22,7 @@ export class JwtInterceptorService implements HttpInterceptor {
   ): Observable<HttpEvent<any>> {
     let token: String = this.loginService.userToken;
 
-    if (token != '') {
+    if (token) {
       req = req.clone({
         setHeaders: {
           'Content-Type': 'application/json;charset=utf-8',
@@ -29,6 +31,16 @@ export class JwtInterceptorService implements HttpInterceptor {
         },
       });
     }
-    return next.handle(req);
+
+    return next.handle(req).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401 || error.status === 403) {
+          // Si el token ha expirado o el usuario no está autorizado, redirige al inicio
+          this.router.navigate(['/inicio']);
+          this.loginService.logout(); // Limpia el token y cualquier sesión del usuario si tienes un método de logout
+        }
+        return throwError(error); // Lanza el error para que otros manejadores también puedan procesarlo
+      })
+    );
   }
 }
