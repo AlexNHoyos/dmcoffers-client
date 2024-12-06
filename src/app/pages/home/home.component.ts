75ebdd66bd63b86e0a541d5cd220fff329f9bf5c
Ramiro Ceballos <rceballos.work@gmail.com>
2024-12-05 21:46:26 -0300

    mostrar los juegos sin loguear

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,22 +10,9 @@ import { LoginService } from 'src/app/services/auth/login.service';
 
 export class HomeComponent implements OnInit {
 
-  userLoginOn: boolean = false;
-  private subscription: Subscription = new Subscription();
-
-  constructor(private loginService: LoginService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.subscription.add(
-      this.loginService.userLoginOn.subscribe({
-        next: (userLoginOn) => {
-          this.userLoginOn = userLoginOn;
-        },
-        error: (err) => {
-          console.error('Error al suscribirse al estado de login', err);
-        },
-      })
-    );
   }
 }
 
