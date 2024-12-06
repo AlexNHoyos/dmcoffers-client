5aacb4008901627ef320971549ea4f9112f0237a
Ramiro Ceballos <rceballos.work@gmail.com>
2024-12-05 21:41:03 -0300

    mostrar el menu en todas las pantallas

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { LoginService } from 'src/app/services/auth/login.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+
+export class HomeComponent implements OnInit {
+
+  userLoginOn: boolean = false;
+  private subscription: Subscription = new Subscription();
+
+  constructor(private loginService: LoginService) { }
+
+  ngOnInit(): void {
+    this.subscription.add(
+      this.loginService.userLoginOn.subscribe({
+        next: (userLoginOn) => {
+          this.userLoginOn = userLoginOn;
+        },
+        error: (err) => {
+          console.error('Error al suscribirse al estado de login', err);
+        },
+      })
+    );
+  }
+}
+
+
