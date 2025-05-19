c38435afec0f2f20ea5570bfb3d048863b86a307
Ramiro Ceballos <rceballos.work@gmail.com>
2025-05-19 20:05:12 -0300

    actualizacion a develop

--- src/app/components/sweitemmenu/sweitemmenu.component.ts
+++ src/app/components/sweitemmenu/sweitemmenu.component.ts
@@ -20,7 +20,6 @@ export class SweItemMenuComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadMenuItems();
-
   }
 
   loadMenuItems(): void {
@@ -58,6 +57,8 @@ export class SweItemMenuComponent implements OnInit {
       })
 
       this.menuItems = menuItems;
+
+      console.log(menuItems);
     });
   }
 
